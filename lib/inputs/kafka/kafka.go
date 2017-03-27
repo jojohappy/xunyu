e1e2d0866d091db97e08c6857954f04c1498c660
jojohappy <sarahdj0917@gmail.com>
2017-03-27 21:52:05 +0800

    Add new input kafka

---
+++ lib/inputs/kafka/kafka.go
@@ -0,0 +1,108 @@
+package kafka
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/xunyu/common"
+	"github.com/xunyu/config"
+
+	"github.com/Shopify/sarama"
+	"github.com/wvanbergen/kafka/consumergroup"
+	"github.com/wvanbergen/kazoo-go"
+)
+
+type KafkaConfig struct {
+	GroupId   string `config:"groupId"`
+	Topics    string `config:"topics"`
+	Zookeeper string `config:"zookeeper"`
+	Initial   int64  `config:"initial"`
+}
+
+type kafka struct {
+	common.PluginPrototype
+	config KafkaConfig
+}
+
+var (
+	defaultConfig = KafkaConfig{
+		GroupId: "xunyu-kafka",
+		Initial: sarama.OffsetOldest,
+	}
+)
+
+func init() {
+	common.RegisterInputPlugin("kafka", New)
+}
+
+func New(config *config.Config) (common.Pluginer, error) {
+	k := &kafka{config: defaultConfig}
+	if err := k.init(config); nil != err {
+		return nil, err
+	}
+	return k, nil
+}
+
+func (k *kafka) init(config *config.Config) error {
+	if err := config.Assemble(&k.config); nil != err {
+		return err
+	}
+
+	fmt.Println(k.config)
+
+	if _, err := k.newKafkaConfig(); nil != err {
+		return err
+	}
+
+	return nil
+}
+
+func (k *kafka) newKafkaConfig() (*consumergroup.Config, error) {
+	cfg := consumergroup.NewConfig()
+	if k.config.Initial != 0 {
+		cfg.Offsets.Initial = k.config.Initial
+	}
+
+	return cfg, nil
+}
+
+func newKafkaClient(cfg *consumergroup.Config, zookeeper string, topics string, groupId string) (*consumergroup.ConsumerGroup, error) {
+	var zookeepers []string
+	zookeepers, cfg.Zookeeper.Chroot = kazoo.ParseConnectionString(zookeeper)
+	tp := strings.Split(topics, ",")
+	return consumergroup.JoinConsumerGroup(groupId, tp, zookeepers, cfg)
+}
+
+func (k *kafka) Start() <-chan common.DataInter {
+	out := make(chan common.DataInter, 1)
+	cfg, err := k.newKafkaConfig()
+
+	if nil != err {
+		close(out)
+		return out
+	}
+
+	consumer, err := newKafkaClient(cfg, k.config.Zookeeper, k.config.Topics, k.config.GroupId)
+
+	if nil != err {
+		close(out)
+		return out
+	}
+
+	go func() {
+		for {
+			select {
+			case msg := <-consumer.Messages():
+				out <- string(msg.Value)
+				consumer.CommitUpto(msg)
+			}
+		}
+
+		close(out)
+	}()
+	return out
+}
+
+func (*kafka) Close() error {
+	return nil
+}
--- lib/plugins/plugins.go
+++ lib/plugins/plugins.go
@@ -1,14 +1,15 @@
 package plugins
 
 import (
-    "github.com/xunyu/config"
-    "github.com/xunyu/common"
+	"github.com/xunyu/common"
+	"github.com/xunyu/config"
 
 	// load support channels
 	_ "github.com/xunyu/lib/channels/json"
 
 	// load support inputs
 	_ "github.com/xunyu/lib/inputs/file"
+	_ "github.com/xunyu/lib/inputs/kafka"
 
 	// load support outputs
 	// _ "github.com/xunyu/outputs/elasticsearch"
@@ -16,14 +17,14 @@ import (
 )
 
 func LoadPlugins(
-    inputsConfigs map[string]*config.Config,
-    outputsConfigs map[string]*config.Config,
-    channelsConfigs map[string]*config.Config,
+	inputsConfigs map[string]*config.Config,
+	outputsConfigs map[string]*config.Config,
+	channelsConfigs map[string]*config.Config,
 ) (*common.Plugins, error) {
-    configs := map[string]map[string]*config.Config{
-        "input": inputsConfigs,
-        "output": outputsConfigs,
-        "channel": channelsConfigs,
-    }
-    return common.InitPlugin(configs)
+	configs := map[string]map[string]*config.Config{
+		"input":   inputsConfigs,
+		"output":  outputsConfigs,
+		"channel": channelsConfigs,
+	}
+	return common.InitPlugin(configs)
 }
